2b6271a0945caa2f650ce75f611dcc92bb205ebf
Tiffany <tiffanyswong314@gmail.com>
2023-12-24 15:42:16 -0800

    update handleCancel and link to seat reservation

--- front-end/src/reservations/ListReservations.js
+++ front-end/src/reservations/ListReservations.js
@@ -14,25 +14,16 @@ export default function ListReservations({ reservation, loadDashboard }) {
   if (!reservation || reservation.status === "finished") return null;
 
   // Function to handle cancellation of a reservation
-  function handleCancel() {
-    if (
-      window.confirm(
-        "Do you want to cancel this reservation? This cannot be undone."
-      )
-    ) {
-      const abortController = new AbortController();
-      const reservationToCancel = reservation.reservation_id;
-
-      // Update reservation status to 'cancelled' and reload dashboard data
-      updateReservationStatus(
-        reservationToCancel,
-        "cancelled",
-        abortController.status
-      ).then(loadDashboard);
-
-      return () => abortController.abort();
+  const handleCancel = async () => {
+    if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
+      try {
+        await updateReservationStatus(reservation.reservation_id, "cancelled");
+        loadDashboard(); // Reload dashboard data after cancellation
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
+  };
 
   return (
     <tr style={{ fontFamily: "Rubik", color: "white" }}>
@@ -41,17 +32,10 @@ export default function ListReservations({ reservation, loadDashboard }) {
       <td className="text-center">{reservation.first_name}</td>
       <td className="text-center">{reservation.last_name}</td>
       <td className="text-center">{reservation.mobile_number}</td>
-      <td className="text-center">
-        {reservation.reservation_date.substr(0, 10)}
-      </td>
-      <td className="text-center">
-        {reservation.reservation_time.substr(0, 5)}
-      </td>
+      <td className="text-center">{reservation.reservation_date.substr(0, 10)}</td>
+      <td className="text-center">{reservation.reservation_time.substr(0, 5)}</td>
       <td className="text-center">{reservation.people}</td>
-      <td
-        className="text-center"
-        data-reservation-id-status={reservation.reservation_id}
-      >
+      <td className="text-center" data-reservation-id-status={reservation.reservation_id}>
         {reservation.status}
       </td>
 
@@ -78,11 +62,11 @@ export default function ListReservations({ reservation, loadDashboard }) {
           </td>
 
           <td className="text-center">
-            <a href={`/reservations/${reservation.reservation_id}/seat`}>
+            <Link to={`/reservations/${reservation.reservation_id}/seat`}>
               <button className="btn btn-sm btn-success" type="button">
                 Seat
               </button>
-            </a>
+            </Link>
           </td>
         </>
       )}
